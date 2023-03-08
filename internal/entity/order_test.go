278fca087e71c9e27c429dbe8a23112a74f0eb0f
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 11:15:07 -0300

    test: Order entity test

--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -14,3 +14,14 @@ func Test_IF_Its_Gets_An_Error_If_ID_Is_Black(t *testing.T) {
 		t.Error("Expected error, got nil")
 	}
 }
+
+func Test_IF_Its_Gets_An_Error_If_Price_Is_Black_Or_Less_Than_Zero(t *testing.T) {
+	order := Order{ID: "123", Price: 0}
+	assert.Error(t, order.Validate(), "invalid price")
+
+}
+func Test_IF_Its_Gets_An_Error_If_Tax_Is_Black_Or_Less_Than_Zero(t *testing.T) {
+	order := Order{ID: "123", Price: 0}
+	assert.Error(t, order.Validate(), "invalid tax")
+
+}
