6790c3aba94d38488b08866179f28e4716e4bdcb
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 12:16:21 -0300

    chore: entity ordewr interface

---
+++ internal/infra/database/order_repository_test.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/devfullcycle/gointesivo2/internal/entity"
+	"github.com/stretchr/testify/suite"
+)
+
+type OrderRepositoryTestSuite struct {
+	suite.Suite
+	Db *sql.DB
+}
+
+func (suite *OrderRepositoryTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	suite.NoError(err)
+	db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
+	suite.Db = db
+}
+
+func (suite *OrderRepositoryTestSuite) TearDownSuite() {
+	suite.Db.Close()
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(OrderRepositoryTestSuite))
+}
+
+func (suite *OrderRepositoryTestSuite) TestSavingOrder() {
+	order, err := entity.NewOrder("123", 10.0, 1.0)
+
+	suite.NoError(err)
+	suite.NoError(order.CalculateFinalPrice())
+}
