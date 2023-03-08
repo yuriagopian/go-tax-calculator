0252cd0233d91bfff1bd8992265b1aff08c675db
yuriagopian <yuri.agopian@hotmail.com>
2023-03-07 23:30:49 -0300

    chore: initial file

--- main.go
+++ main.go
@@ -2,32 +2,14 @@ package main
 
 import "fmt"
 
-type Order struct {
-	ID       string
-	Price    float64
-	Quantity int
-}
-
-func (o Order) SetPrice(price float64) {
-	o.Price = price
-	fmt.Println("Price dentro do setPrice: ", o.Price)
-}
-
-func (o Order) getTotal() float64 {
-	return o.Price * float64(o.Quantity)
-}
-
 func main() {
-	name := "Yuri"
-	name = "Yuri Agopian"
-	fmt.Println("Hello,", name)
 
-	order := Order{
-		ID:       "123",
-		Price:    10.0,
-		Quantity: 5,
-	}
+	a := 10
+	b := a
+	fmt.Println(b)
 
-	order.SetPrice(20.0)
-	fmt.Println(order.getTotal())
+	b = 20
+	fmt.Println(a)
+	fmt.Println(b)
+
 }
