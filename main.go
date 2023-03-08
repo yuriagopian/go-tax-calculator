0fca3b64ff18d337b9523e77349ffe8735c0204a
yuriagopian <yuri.agopian@hotmail.com>
2023-03-07 23:21:50 -0300

    chore: initial file

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
+import "fmt"
+
+type Order struct {
+	ID       string
+	Price    float64
+	Quantity int
+}
+
+func (o Order) getTotal() float64 {
+	return o.Price * float64(o.Quantity)
+}
+
 func main() {
 	name := "Yuri"
 	name = "Yuri Agopian"
 	print("Hello, world! ", name)
+	fmt.Println(name)
 }
