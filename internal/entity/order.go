6893b903e254b965e88f45a771eab11cbb86dae3
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 10:44:43 -0300

    feat: Order entity

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -1,8 +1,26 @@
 package entity
 
+import "errors"
+
 type Order struct {
 	ID         string
 	Price      float64
 	Tax        float64
 	FinalPrice float64
+}
+
+func (o *Order) Validate() error {
+	if o.ID == "" {
+		return errors.New("id is required")
+	}
+
+	if o.Price <= 0 {
+		return errors.New("invalid Price value")
+	}
+
+	if o.Tax <= 0 {
+		return errors.New("invalid Tax value")
+	}
+
+	return nil // erro nulo
 }
