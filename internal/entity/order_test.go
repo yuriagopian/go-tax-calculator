f8299d51f3d7a3b39b94c3829a321e0e10e336d0
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 11:12:09 -0300

    test: Order entity test

--- internal/entity/order_test.go
+++ internal/entity/order_test.go
@@ -1,9 +1,14 @@
 package entity
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_IF_Its_Gets_An_Error_If_ID_Is_Black(t *testing.T) {
 	order := Order{}
+	assert.Error(t, order.Validate(), "invalid id")
 	err := order.Validate()
 	if err == nil {
 		t.Error("Expected error, got nil")
