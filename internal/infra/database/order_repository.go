10c33898c920053d5bf621b8ad5e67377cb4b723
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 11:27:07 -0300

    chore: entity ordewr interface

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -1,7 +1,24 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/devfullcycle/gointesivo2/internal/entity"
+)
 
 type OrderRepository struct {
 	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{Db: db}
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	_, err := r.Db.Exec("INSERT INTO orders(id, price, tax, final_price) Values(?,?,?,?)", order.ID, order.Price, order.Tax, order.FinalPrice)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
